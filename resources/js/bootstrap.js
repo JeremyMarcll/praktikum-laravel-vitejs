b948e9803d2d2462dca96b118a51d35692e98d7e
jeremylatihan <142556200+JeremyMarcll@users.noreply.github.com>
2025-11-16 16:54:28 +0700

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,6 @@
 // ...existing code...
 import axios from "axios";
 window.axios = axios;
-window.axios.defaults.withCredentials = true;
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 const token = document
     .querySelector('meta[name="csrf-token"]')
